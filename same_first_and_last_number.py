8110b6c17005b30929ff5f9d5297c43dcc50a38f
yhasnayafranchette <yhasnayafranchettesoriano@gmail.com>
2024-01-22 19:29:00 +0800

    if else

--- same_first_and_last_number.py
+++ same_first_and_last_number.py
@@ -1,3 +1,11 @@
 #Exercise 5: Check if the first and last number of a list is the same
 #Write a function to return True if the first and last number of a given list is same. If numbers are different then return False.
 
+# Compare the first and last numbers
+def first_and_last_number(number_list):
+    if first_and_last_number:
+        return first_and_last_number[0] == first_and_last_number[-1]
+    else:
+        return False
+
+#Print output
