b2b31dee4261397de64dffb347049d3522a4c767
yhasnayafranchette <yhasnayafranchettesoriano@gmail.com>
2024-01-22 19:45:21 +0800

    revise

--- same_first_and_last_number.py
+++ same_first_and_last_number.py
@@ -8,14 +8,14 @@ def first_and_last_number(number_list):
     first_number = number_list[0]
     last_number = number_list[-1]
 
-    if first_and_last_number:
-        return first_and_last_number[0] == first_and_last_number[-1]
+    if first_number[0] == last_number[-1]:
+        return True
     else:
         return False
 
 #Print output
 numbers_x = [10, 20, 30, 40, 10]
-print("For numbers_x:", numbers_x)
+print("For numbers_x:", first_and_last_number(numbers_x))
 
 numbers_y = [75, 65, 35, 75, 30]
-print("For numbers_y:", numbers_y) 
+print("For numbers_y:", first_and_last_number(numbers_y)) 
